97170a33fbe539eaab2965773f1e1d97babc2236
4513ECHO <4513echo@gmail.com>
2021-07-26 01:27:42 +0000

    [add] 完成品を追加

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # discord-ip-notifier
+
+systemdを使って1時間ごとにRPiのグローバルIPアドレスをdiscordに送信するプログラム
+
+## Usage
+
+### 準備
+
+```shell
+sudo systemctl link /home/hibiki/Develops/discord-ip-notifier/notifier.service
+sudo systemctl link /home/hibiki/Develops/discord-ip-notifier/notifier.timer
+sudo systemctl enable /home/hibiki/Develops/discord-ip-notifier/notifier.service
+sudo systemctl enable /home/hibiki/Develops/discord-ip-notifier/notifier.timer
+```
+
+### 起動/停止
+
+```shell
+sudo systemctl start notifier
+sudo systemctl stop notifer
+```
---
+++ main.py
@@ -0,0 +1,20 @@
+from urllib import request
+import json
+import subprocess
+
+webhook_url = os.getenv('WEBHOOK_URL')
+
+command = 'curl global.me'
+
+raw_result = subprocess.run(command.split(), stdout=subprocess.PIPE).stdout
+result = raw_result.decode('utf-8').replace('\n', '')
+
+main_content = json.dumps({
+    'username': 'global IP notifier',
+    'avatar_url': 'https://image.freepik.com/free-icon/ip-address_318-1438.jpg',
+    'content': result
+}).encode('utf-8')
+
+headers = {'Content-Type': 'application/json'}
+
+req = request.Request(webhook_url, data=main_conten, headers=headers,  method='POST')
